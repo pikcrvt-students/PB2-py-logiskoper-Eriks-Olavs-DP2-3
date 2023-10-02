cc5510cc9e4a2c2f565ccb5da0fd3031018d3ce8
Eriks Olavs <eo00789@rvt.lv>
2023-10-02 13:41:38 +0300

    first messsage

--- main1.py
+++ main1.py
@@ -1,3 +1,15 @@
+import doctest
+def parb(x):
+    """
+    >>> parb(10)
+    Ir intervala
+    >>> parb(-2)
+    Ir intervala
+    >>> parb(3,9)
+    Nav intervala
+    """
+
+
 x = int(input('x:'))
 
 if x > 10 and x < 50:
