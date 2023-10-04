4f634f59b83ab3e00199495d34d15852cac1ae61
Eriks Olavs <eo00789@rvt.lv>
2023-10-04 12:26:58 +0300

    first messsage

--- main4.py
+++ main4.py
@@ -1,24 +1,25 @@
 import doctest
-def parbaudit_koord(koord_x:float) -> bool:
+def parbaudit_koord(koord_x:float):
     """
-    Funkcijas parbaude x koordinati.
-    >>> parbaudit_koord(1.3)
+    Funkcija pārbauda x koordināti . x pieder (3.9;+ infinit
+    >>> parbaudit_koord(3)
     False
-    >>> parbaudit_koord(3.9)
+    >>> parbaudit_koord(-1)
     False
-    >>> parbaudit_koord(9)
+    >>> parbaudit_koord(7)
     True
-
     """
     if koord_x > 3.9:
         return True
     else:
         return False
-doctest.testmod(verbose=True)
 
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
 
-vertiba = float(input('ievadiet x koordinati: '))
-if parbaudit_koord(vertiba):
-    print('Skaitlis ir diapazona')
-else:
-    print('Skaitlis NAV diapazona')
+    vertiba = float(input("Ievadiet x koordināti: "))
+
+    if parbaudit_koord(vertiba):
+        print("Skaitlis ir diapazonā")
+    else:
+        print("Skaitlis nav diapazonā")
