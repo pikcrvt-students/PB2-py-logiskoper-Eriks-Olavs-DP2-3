d8ef05aef59177c5f898d41f9ac83576eeff040b
Eriks Olavs <eo00789@rvt.lv>
2023-10-18 14:11:29 +0300

    gatava programma lai parbaudit koordinates 3 sturi.

--- uzd9.py
+++ uzd9.py
@@ -1,2 +1,8 @@
-x = int(input('Ievadiet x koordinatus: '))
-y = int(input('Ievadiet y koordinatus: '))
+x = float(input('ievadiet x koordinati:'))
+y = float(input('ievadiet y koordinati:'))
+
+if (0 <= x <= 2) or (0 <= y <= 3):
+    if (x == 0 and (0 <= y <= 3)) or (y == 0 and 0 <= x <= 2) or y == -1.5 * x + 3 and 0 < x < 2:
+        print('Punkts ir robežas līnijā')
+else:
+    print('Punkts ir figuras iekšā')
